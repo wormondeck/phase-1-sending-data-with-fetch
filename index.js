abce374fb6ca153bab67d5ac95bb7676f99231cd
Ronney Arias <ronneyarias@gmail.com>
2023-10-30 12:10:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com",
+    }),
+};
+    return fetch("http://localhost:3000/users", configObject)
+    .then(response => response.json())
+    .then(data => {
+        // Retrieve the new ID value from the response data
+        const newId = data.id;
+  
+        // Append the new ID to the DOM
+        const idElement = document.createElement("p");
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement);
+      })
+    .catch(error => {
+        // Handle the error
+        const errorElement = document.createElement("p");
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+    });
+}
+submitData("Steve", "steve@steve.com");
